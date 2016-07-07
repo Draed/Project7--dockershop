92465ae2c67c68e69b65a4e1f148ae0ea277db26
Draed <draedper@gmail.com>
2016-07-07 18:05:43 +0400

    Starting implementation of localapp
    I started but , just started nothing really done for the moment
    TODO first of all : Create an entity in latest bundle (localrepo) as
    localapp.
    a local app is same as an app in the shop unless it is local
    (obviously).
    
    I just finish my week ! I’m not going to work on for the next 3 days,
    rendez-vous prévu Monday

--- src/LocalRepoBundle/Controller/Push.php
+++ src/LocalRepoBundle/Controller/Push.php
@@ -3,11 +3,19 @@
 namespace LocalRepoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Docker\Docker;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
-        return $this->render('LocalRepoBundle:Default:index.html.twig');
+
+
+		$docker = new Docker();
+		$containers = $docker->getContainerManager()->findAll();
+		return $this->render('LocalRepoBundle:Default:index.html.twig', array(
+            'containers' => $containers,
+        ));
+
     }
 }
